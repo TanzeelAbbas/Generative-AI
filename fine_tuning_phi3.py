5d4d399da782c7a8bc4af23997bac34954f81e9e
Tanzeel Abbas <72796092+TanzeelAbbas@users.noreply.github.com>
2024-05-30 17:37:53 +0500

    Update fine_tuning_phi3.py

--- fine_tuning_phi3.py
+++ fine_tuning_phi3.py
@@ -25,42 +25,46 @@ class FineTuning:
         # Dropout rate for LoRA layers
         self.lora_dropout = lora_dropout
 
-    def load_data(self, file_path):
-        dataset = pd.read_csv(file_path)
-        # For the chat model(LLM) we convert the dataset to below format for training.
-        # Note: Below is specific format for llama model fine tuning for other it may be different format       
-        dataset['text'] = dataset['Questions'] + dataset['Answers']
-        dataset = Dataset.from_pandas(dataset[['text']])
-        return dataset
-
+    def load_data(self, train_path, test_path):
+        train_dataset = pd.read_csv(train_path)
+        test_dataset = pd.read_csv(test_path)        
+        train_dataset['text'] = "<|user|>\n" + train_dataset['Questions'] + "<|end|>\n" + "<|assistant|>\n" + train_dataset['Answers']
+        test_dataset['text'] = "<|user|>\n" + test_dataset['Questions'] + "<|end|>\n" + "<|assistant|>\n" + test_dataset['Answers']
+        
+        train_dataset = Dataset.from_pandas(train_dataset[['text']])
+        test_dataset = Dataset.from_pandas(test_dataset[['text']])
+        return train_dataset, test_dataset
+    
+    def setup_lora(self):
+        lora_config = LoraConfig(
+            lora_alpha=self.lora_alpha,
+            lora_dropout=self.lora_dropout,
+            target_modules = ["q_proj", "k_proj", "v_proj", "o_proj","gate_proj", "up_proj", "down_proj",],
+            r=self.lora_r,
+            bias="none",
+            task_type="CAUSAL_LM",
+        )
+        return lora_config
 
     def setup_model(self):
-        os.environ["HF_TOKEN"] = "PASTE YOUR HUGGINGFACE API KEY HERE"
+        os.environ["HF_TOKEN"] = "PASTE YOUR HUGGING FACE API TOKEN HERE"
         model = AutoModelForCausalLM.from_pretrained(
             self.model_name,
             torch_dtype=torch.float16,
             token=os.environ["HF_TOKEN"],
+            trust_remote_code=True
         )
         model.config.use_cache = False
         model.config.pretraining_tp = 1
-        return model
+        lora_model = get_peft_model(model, self.setup_lora())
+
+        return lora_model
 
     def setup_tokenizer(self):
         tokenizer = AutoTokenizer.from_pretrained(self.model_name, trust_remote_code=True, token=os.environ["HF_TOKEN"],)
         tokenizer.pad_token = tokenizer.eos_token
         tokenizer.padding_side = "right"
         return tokenizer
-
-    def setup_lora(self):
-        lora_config = LoraConfig(
-            lora_alpha=self.lora_alpha,
-            lora_dropout=self.lora_dropout,
-            target_modules="all-linear",
-            r=self.lora_r,
-            bias="none",
-            task_type="CAUSAL_LM",
-        )
-        return lora_config
 
     def setup_training_arguments(self):
         # Training arguments
@@ -71,16 +75,18 @@ class FineTuning:
             overwrite_output_dir=True,
             # Number of epochs to train for
             num_train_epochs=5,
+            # we can also use 'steps'
+            evaluation_strategy="steps", 
             # Batch size per device during training
             per_device_train_batch_size=4,
             # Number of gradient accumulation steps
             gradient_accumulation_steps=1,
             # Optimizer to use
-            optim="adamw_torch",
+            optim="paged_adamw_32bit",
             # Number of steps to save the model checkpoint
-            save_steps=25,
+            save_steps=5000,
             # Number of steps to log metrics
-            logging_steps=25,
+            logging_steps=1000,
             # Initial learning rate
             learning_rate=2e-4,
             # Weight decay for regularization
@@ -104,10 +110,11 @@ class FineTuning:
         )
         return training_arguments
 
-    def setup_trainer(self, model, tokenizer, lora_config, train_dataset):
+    def setup_trainer(self, model, tokenizer, lora_config, train_dataset, test_dataset):
         trainer = SFTTrainer(
             model=model,
             train_dataset=train_dataset,
+            eval_dataset=test_dataset,
             peft_config=lora_config,
             dataset_text_field="text",
             max_seq_length=1024,
@@ -117,11 +124,11 @@ class FineTuning:
         )
         return trainer
 
-    def train(self, train_dataset, fine_tuned_model_name):
+    def train(self, train_dataset,test_dataset, fine_tuned_model_name):
         model = self.setup_model()
         tokenizer = self.setup_tokenizer()
         lora_config = self.setup_lora()
-        trainer = self.setup_trainer(model, tokenizer, lora_config, train_dataset)
+        trainer = self.setup_trainer(model, tokenizer, lora_config, train_dataset, test_dataset)
         train_result = trainer.train()
         metrics = train_result.metrics
         trainer.log_metrics("train", metrics)
@@ -132,16 +139,15 @@ class FineTuning:
         fine_tuned_model = fine_tuned_model_name
         trainer.model.save_pretrained(fine_tuned_model)
         return trainer
-
+    
 
 if __name__ == "__main__":
-    model_name = "microsoft/Phi-3-mini-128k-instruct"
-    ft = FineTuning(model_name, "/content/results", 64, 16, 0.1) # "meta-llama/Meta-Llama-3-8B-Instruct"
-    dataset = ft.load_data("train.csv")
-    fine_tuned_model_name = "/content/Phi-3-mini-128k-instruct-finetune"
-    ft.train(dataset, fine_tuned_model_name)
-
-
+    model_name = "microsoft/Phi-3-mini-4k-instruct"
+    ft = FineTuning(model_name, "Phi-3-mini-4k", 64, 16, 0.1) # "meta-llama/Meta-Llama-3-8B"
+    train_dataset, test_dataset = ft.load_data("jira_QA.csv", "validation_data.csv")
+    fine_tuned_model_name = "Phi-3-mini-4k-instruct-fine-tuned"
+    ft.train(train_dataset, test_dataset, fine_tuned_model_name)    
+    
 
 # Check the plots on tensorboard, as follows
 
